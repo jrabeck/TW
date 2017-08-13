2e6405e877e48e9d35876c49be92c6c7d5fa7edd
Jake Rabeck <Jakerabeck@gmail.com>
2017-08-12 22:18:03 -0700

    clean after hangout

--- New/cashRegister.java
+++ New/cashRegister.java
@@ -15,13 +15,12 @@ import java.io.IOException;
 // or at least not until I'm ready to get fancy. 
 
 class Item {
-  // yes, i know this is abbhorent having everything public
-    public String name;
-    public double price;
-    public boolean exemption;
-    public String newName;
-    public int quantity;
-    public boolean duty; 
+    private String name;
+    private double price;
+    private boolean exemption;
+    private String newName;
+    private int quantity;
+    private boolean duty; 
 
     public Item(String name, double price, boolean exemption, int quantity, boolean duty) {
         this.name = name;
@@ -95,86 +94,85 @@ public class cashRegister {
             while ((itemDesc = reader.readLine()) != null) { 
               
                 if (itemDesc.contains("Input")) {
-                  if (runningTotal != 0.0) {
-                    System.out.println("Sales Taxes: " + runningTax);
-                    System.out.println("Total: " + runningTotal);
-                    System.out.println();
-                  }
-                  runningTotal = 0.0;
-                  runningTax = 0.0;
-                  customerNumber = customerNumber + 1;
-                  System.out.println("Output: " + customerNumber);
+                    if (runningTotal != 0.0) {
+                      System.out.println("Sales Taxes: " + runningTax);
+                      System.out.println("Total: " + runningTotal);
+                      System.out.println();
+                    }
+                    runningTotal = 0.0;
+                    runningTax = 0.0;
+                    customerNumber = customerNumber + 1;
+                    System.out.println("Output: " + customerNumber);
                 }
 
                 else if (itemDesc.isEmpty()) {
-                  continue;
+                    continue;
                 }
 
                 else {
-              // itemDesc is the string of a itemDesc as a whole, words is the itemDesc
-              // as an object, current is the current word as a string. 
-                      Item newItem = new Item("", 0, false, 0, false);                 
-                      boolean isExempt = exemptFind(itemDesc, exempt);
-                      boolean hasADuty = dutyFind(itemDesc, imported);
-                      StringTokenizer words = new StringTokenizer(itemDesc);
-              
-                      System.out.println(itemDesc);
-                      // this whole first character thing I wrote to help with the quantity
-                      // problem, to have firstCharacter to use in a method
-                      String firstCharacter = words.nextToken();
-                      //  while (words.hasMoreTokens()) {  
-                      //  String currentt = words.nextToken();
-                      //  if (currentt == "a") {
-                      //   currentt = ":";
-                      //  }       
-                      // }
-                      // System.out.println(firstCharacter);
-                      // so what's happening is it's going to the first character and seeing
-                      // that it ("I") isn't int convertable, and this is breaking the try 
-                      // method, which even though I don't understadn try really, i get. it's an
-                      // exception that it catches. I 
-                      // can't do any code after the final because of this. still compiles
-                      // for some reason. 
-                      int quantity = Integer.parseInt(firstCharacter); 
-                      newItem.setQuantity(quantity);
-                      Double currentPrice = newItem.price;
-                      if (isExempt == true) {
-                          newItem.setExemption(isExempt);
-                      }
-                      if (hasADuty == true) {
-                          newItem.setDuty(hasADuty);
-                      }
-                      while (words.hasMoreTokens()) {         
-                      String current = words.nextToken();
+                    // itemDesc is the string of a itemDesc as a whole, words is the itemDesc
+                    // as an object, current is the current word as a string. 
+                    Item newItem = new Item("", 0, false, 0, false);                 
+                    boolean isExempt = exemptFind(itemDesc, exempt);
+                    boolean hasADuty = dutyFind(itemDesc, imported);
+                    StringTokenizer words = new StringTokenizer(itemDesc);
+                    System.out.println(itemDesc);
+                    // this whole first character thing I wrote to help with the quantity
+                    // problem, to have firstCharacter to use in a method
+                    String firstCharacter = words.nextToken();
+                    //  while (words.hasMoreTokens()) {  
+                    //  String currentt = words.nextToken();
+                    //  if (currentt == "a") {
+                    //   currentt = ":";
+                    //  }       
+                    // }
+                    // System.out.println(firstCharacter);
+                    // so what's happening is it's going to the first character and seeing
+                    // that it ("I") isn't int convertable, and this is breaking the try 
+                    // method, which even though I don't understadn try really, i get. it's an
+                    // exception that it catches. I 
+                    // can't do any code after the final because of this. still compiles
+                    // for some reason. 
+                    int quantity = Integer.parseInt(firstCharacter); 
+                    newItem.setQuantity(quantity);
+                    Double currentPrice = newItem.getPrice();
+                    if (isExempt == true) {
+                        newItem.setExemption(isExempt);
+                    }
+                    if (hasADuty == true) {
+                        newItem.setDuty(hasADuty);
+                    }
+                    while (words.hasMoreTokens()) {         
+                    String current = words.nextToken();
 
                         if (current.contains(".")) {
-                          currentPrice = Double.parseDouble(current);
-                          newItem.setPrice(currentPrice);
+                            currentPrice = Double.parseDouble(current);
+                            newItem.setPrice(currentPrice);
 
                             if (newItem.getExemption() == false) {
-                              Double tax = (newItem.price * 0.1);
-                              Double roundTax = Math.round(tax*100.0)/100.0;
-                              Double afterTax = newItem.price + roundTax;
-                              afterTax = Math.round(afterTax*100.0)/100.0;
-                              newItem.setPrice(afterTax * quantity);
-                              runningTax = (runningTax + tax);
+                                Double tax = (newItem.getPrice() * 0.1);
+                                Double roundTax = Math.round(tax*100.0)/100.0;
+                                Double afterTax = newItem.getPrice() + roundTax;
+                                afterTax = Math.round(afterTax*100.0)/100.0;
+                                newItem.setPrice(afterTax * quantity);
+                                runningTax = (runningTax + tax);
                             }
                                 else {
-                                  newItem.setPrice(currentPrice * quantity);
+                                    newItem.setPrice(currentPrice * quantity);
                                 }
 
                             if (newItem.getDuty() == true) {
-                              Double duty = (newItem.price * 0.05);
-                              Double roundDuty = Math.round(duty*100.0)/100.0;
-                              Double afterDuty = newItem.price + roundDuty;
-                              afterDuty = Math.round(afterDuty*100.0)/100.0;
-                              newItem.setPrice(afterDuty);
-                              runningTax = (runningTax + duty);
+                                Double duty = (newItem.getPrice() * 0.05);
+                                Double roundDuty = Math.round(duty*100.0)/100.0;
+                                Double afterDuty = newItem.getPrice() + roundDuty;
+                                afterDuty = Math.round(afterDuty*100.0)/100.0;
+                                newItem.setPrice(afterDuty);
+                                runningTax = (runningTax + duty);
                             }
-                          runningTotal = runningTotal + (newItem.getPrice());
+                            runningTotal = runningTotal + (newItem.getPrice());
                         }
 
-                      }
+                    }
                   
                 }
                 
@@ -196,23 +194,22 @@ public class cashRegister {
     } 
 
 
-  public static boolean exemptFind(String word, String[] taxExempt) {
-      for(int i = 0; i < (taxExempt.length); i++) {
-          if (word.contains(taxExempt[i])) {
-              return true;
-          }
-      }
-      return false;
-  }
+    public static boolean exemptFind(String word, String[] taxExempt) {
+        for(int i = 0; i < (taxExempt.length); i++) {
+            if (word.contains(taxExempt[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-  public static boolean dutyFind(String wordd, String[] dutyCheck) {
-      for(int i = 0; i < (dutyCheck.length); i++) {
-          if (wordd.contains(dutyCheck[i])) {
-              return true;
-          }
-      }
-      return false;
-  }
+    public static boolean dutyFind(String wordd, String[] dutyCheck) {
+        for(int i = 0; i < (dutyCheck.length); i++) {
+            if (wordd.contains(dutyCheck[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 };
-
